f0e9ce6039e9acf149a945b982d0be3894d029a2
csernakakos <csernakakos@gmail.com>
2022-04-10 08:26:39 +0200

    Testing the backend

--- part3/course-material/utils/config.js
+++ part3/course-material/utils/config.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
-const URL = process.env.MONGODBURL;
+const URL = process.env.NODE_ENV === "test"
+    ? process.env.MONGODBURL_TEST
+    : process.env.MONGODBURL
 
 module.exports = {
     PORT,
